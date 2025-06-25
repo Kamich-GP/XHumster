d9c9e27edec8409c493fec2323de6097e73059b8
Kamich-GP <yurochkagameplus@gmail.com>
2025-06-25 20:23:19 +0500

    forms

--- index/views.py
+++ index/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
-from .models import Product, Category
+from django.shortcuts import render, redirect
+from .models import Product, Category, Cart
+from .forms import RegForm
+from django.views import View
+from django.contrib.auth import login, logout
+from django.contrib.auth.models import User
 
 
 # Create your views here.
@@ -59,3 +63,34 @@ def search(request):
                 'request': get_product
             }
             return render(request, 'result.html', context)
+
+
+# Регистрация
+class Register(View):
+    template_name = 'registration/register.html'
+
+    def get(self, request):
+        context = {'form': RegForm}
+        return render(request, self.template_name, context)
+
+    def post(self, request):
+        form = RegForm(request.POST)
+
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            email = form.cleaned_data.get('email')
+            password = form.cleaned_data.get('password2')
+
+            user = User.objects.create_user(username=username,
+                                            email=email,
+                                            password=password)
+            user.save()
+
+            login(request, user)
+            return redirect('/')
+
+
+# Выход из аккаунта
+def logout_view(request):
+    logout(request)
+    return redirect('/')
